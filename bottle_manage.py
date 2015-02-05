967d8f147e3bb03b68ccf5f409de57acde7dc837
Kirill Klenov <horneds@gmail.com>
2015-02-05 14:14:34 +0300

    Bump version: 0.1.0 → 0.2.0

--- bottle_manage.py
+++ bottle_manage.py
@@ -6,7 +6,7 @@
 # Package information
 # ===================
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __project__ = "bottle-manage"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
